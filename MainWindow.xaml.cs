d4aa70b6f1993fd393895352436aaef256b6175c
Bas Hoekstra <info@bashoekstra.nl>
2020-02-27 15:00:38 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+
+namespace RandomNumberTotal
+{
+
+    public partial class MainWindow : Window
+    {
+        private int generatedNumber;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var random = new Random();
+            generatedNumber = random.Next(5, 100);
+
+            generateTextBlock.Text = $"{generatedNumber}";
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int sum = 0;
+            for (int i = 0; i <= generatedNumber; i++)
+            {
+                sum = sum + i;
+            }
+            sumTextBlock.Text = $"De som van 1 tot en met {generatedNumber} is {sum}";
+        }
+    }
+}
